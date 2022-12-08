d50021793ed6d5164c50d07b5fa67dcb6a538e46
das0166 <das0166@naver.com>
2022-12-08 15:42:31 +0900

    2022.10.08 배경 작업중

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ $(window).on("wheel", function(e) {
     var posTop =(page-1) * $(window).height();
     mHtml.animate({scrollTop : posTop});
 })
+
+// 배경
+
+
+window.onresize = () => {
+    makeStars();
+}
+
+const makeStars = () => {
+    const $sky = document.querySelector(".sky");
+    
+    const maxSize = Math.max(window.innerWidth,window.innerHeight);
+
+    const getRandomX = () => Math.random() * maxSize;
+
+    const getRandomY = () => Math.random() * maxSize;
+
+    const randomRadius = () => Math.random() * 0.7 + 0.6;
+
+    const _size = Math.floor(maxSize / 2);
+  
+  const htmlDummy = new Array(_size).fill().map((_, i) => {
+    return  `<circle class='star'
+      cx=${getRandomX()}
+      cy=${getRandomY()}
+      r=${randomRadius()}
+      className="star" />`
+  }).join('');
+  
+  $sky.innerHTML = htmlDummy;
+}
